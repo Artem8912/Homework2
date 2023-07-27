d02c075eede85b48a34d636bc176d5c962cc0648
Art <artemgoryunov89@gmail.com>
2023-07-27 13:59:38 +0400

    Программа для палиндрома1

---
+++ Exercise-19(1)/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a>99999 || a<10000) 
+{
+Console.WriteLine ("Вы ввели некорректное число");
+} 
+else 
+{
+bool Palindrome(int number)
+{
+    int digit1 = number/10000;
+    int digit2 = number/1000 - digit1*10;
+    int digit4 = (number%100)/10;
+    int digit5 = number%10; 
+    return (digit1 == digit5) && (digit2 == digit4);
+}
+bool result = Palindrome(a);
+Console.WriteLine(result? "Число является палиндромом": "Число не является палиндромом");
+
+} 
