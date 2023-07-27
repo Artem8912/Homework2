4620bc27469e0c6a2da77bbb387a15fcefa2a763
Art <artemgoryunov89@gmail.com>
2023-07-27 16:09:06 +0400

    Программа для вычисления кубов1

---
+++ Exercise-23(1)/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число  ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if( a<1)
+{
+    Console.WriteLine(" Вы ввели некорректное число");
+    return;
+}
+void Cube(int number)
+{
+    for (int i = 1; i<=number;i++)
+    {
+    double cube = Math.Pow(i,3);
+    Console.WriteLine($"{i,3} {cube,7}" );
+    } 
+}
+Cube(a);
